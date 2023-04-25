de25502a3dc5f87abf2f2e4c30f17e80081424b5
Bogdddan <bogdankrajnik95@gmail.com>
2023-04-25 21:15:43 +0300

    gj r

--- src/index.js
+++ src/index.js
@@ -13,61 +13,46 @@ let page = 1;
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   searchQuery = input.value.trim();
-  if (searchQuery !== '') {
-    divForRender.innerHTML = '';
-    page = 1; 
-        fetchCountries(searchQuery, page)
-      .then(data => {
-        renderImages(data.hits);
-        if (data.totalHits <= divForRender.children.length) { 
-          btn.style.display = 'none';
-          Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
-        } else {
-          btn.style.display = 'block';
-        }
-      })
-      .catch(error => console.error('Error:', error));
-  } else {
-    divForRender.innerHTML = '';
-  }
+    if (searchQuery !== '') {
+  divForRender.innerHTML = '';
+  page = 1;
+  fetchCountries(searchQuery, page)
+.then(data => {
+  renderImages(data.hits);
+    if (data.totalHits <= divForRender.children.length) {
+  btn.style.display = 'none';
+  Notiflix.Notify.info("Вибачте, але ви дійшли до кінця результатів пошуку.");
+}  else {
+  btn.style.display = 'block';
+}
+})
+.catch(error => console.error('Помилка:', error));
+} else {
+divForRender.innerHTML = '';
+}
 });
 
 btn.addEventListener('click', () => {
-  page++;
+page++;
   fetchCountries(searchQuery, page)
-    .then(data => {
-      renderImages(data.hits);
-      if (data.totalHits <= divForRender.children.length) { // додано перевірку на кінець колекції
-        btn.style.display = 'none';
-        Notiflix.Notify.info("We're sorry, but you've reached the end of search results.");
-      } else {
-        btn.style.display = 'block';
-      }
-    })
-    .catch(error => console.error('Error:', error));
+  .then(data => {
+  renderImages(data.hits);
+if (data.totalHits <= divForRender.children.length) {
+  btn.style.display = 'none';
+  Notiflix.Notify.info("Вибачте, але ви дійшли до кінця результатів пошуку.");
+} else {
+  btn.style.display = 'block';
+}
+})
+.catch(error => console.error('Помилка:', error));
 });
 
 function renderImages(images) {
-  const imageList = images.map(image => {
-  const { webformatURL, largeImageURL, tags, likes, views, comments, downloads } = image;
-  return `
-  <div class="image-card"> 
-    <img src="${webformatURL}" alt="${tags}" class="image-card__image" data-source="${largeImageURL}"> 
-      <div class="image-card__stats"> 
-          <p class="image-card__stats-item">
-            <i class="material-icons">thumb_up</i> ${likes} 
-          </p> 
-          <p class="image-card__stats-item"> 
-            <i class="material-icons">visibility</i> ${views} 
-          </p> 
-          <p class="image-card__stats-item"> 
-            <i class="material-icons">comment</i> ${comments} 
-          </p> 
-          <p class="image-card__stats-item"> 
-            <i class="material-icons">cloud_download</i> ${downloads} 
-          </p> 
-          </div> 
-      </div>` ;
+const imageList = images.map(image => {
+const { webformatURL, largeImageURL, tags, likes, views, comments, downloads } = image;
+return `
+<div class="image-card"> <img src="${webformatURL}" alt="${tags}" class="image-card__image" data-source="${largeImageURL}"> <div class="image-card__stats"> <p class="image-card__stats-item"> <i class="material-icons">thumb_up</i> ${likes} </p> <p class="image-card__stats-item"> <i class="material-icons">visibility</i> ${views} </p> <p class="image-card__stats-item"> <i class="material-icons">comment</i> ${comments} </p> <p class="image-card__stats-item"> <i class="material-icons">cloud_download</i> ${downloads} </p> </div> </div>
+` ;
 });
-divForRender.innerHTML += imageList.join('');
+  divForRender.innerHTML += imageList.join('');
 }
